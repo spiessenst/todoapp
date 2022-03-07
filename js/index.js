1066ed80e372a1c7812edbb3d40f57ba771a9571
spiessenst <spiessenst@gmail.com>
2022-03-07 16:44:51 +0100

    done

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const todos = [
+    {
+    id: "123456",
+    todo: "Gras afrijden",
+    checked: true,
+    },
+    {
+    id: "423456",
+    todo: "Tv kijken",
+    checked: false,
+    },
+    {
+    id: "12453456",
+    todo: "Auto poetsen",
+    checked: true,
+    },
+    ];
+
+    
